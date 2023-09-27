4e69a302c8bf2298afc279fc70866e44831d2d57
¨Edgar <s.steinberg2019@gmail.com>
2023-09-27 13:07:55 -0300

    citas

---
+++ citas.js
@@ -0,0 +1,38 @@
+let citas = [
+    {
+        'autor': 'Albert Eistein',
+        'texto': 'No poseo talentos especiales . Solo soy apasionadamente curioso'
+    },
+    {
+        'autor': 'Anonimo',
+        'texto': 'Semanas de programacion te pueden ahorras hora de planeacion'
+    },
+    {
+        'autor': 'Alan Kay',
+        'texto': 'La mejor forma de predecir el futuro es inventarlo'
+    },
+    {
+        'autor': 'Amelia Earhart',
+        'texto': 'Lo mas dificil es tomar la decision de actuar. El resto es simplemente tenacidad'
+    },
+    {
+        'autor': 'Benjamin Franklin',
+        'texto': 'Dimelo y lo olvidare. Ensañamelo y lo recordare. Involucrame y lo aprendere'
+    },
+    {
+        'autor': 'Charles R. Swidoll',
+        'texto': 'La vida es 10% lo que te ocurre y 90% como reaccionas'
+    },
+    {
+        'autor': 'Jane Goodall',
+        'texto': 'Lo que hace una diferencia. Y debes decidir si que tipo de diferencia quieres hacer'
+    },
+    {
+        'autor': 'John Muir',
+        'texto': 'El poder de la imaginación nos hace infinitos.'
+    },
+    {
+        'autor': 'Mark Twain',
+        'texto': 'Los dos días más importantes de tu vida son el día que naciste y el día que descubres por qué.'
+      }
+]
